49923371b3de70b45c446c1b759d0828b5143302
nediko <nediko@abv.bg>
2026-07-16 19:22:09 +0300

    chore: remove tracked files that are now in .gitignore

--- d_port/compile.bat
+++
@@ -1,8 +0,0 @@
-REM Optimizer:
-ldc2 -O3 -release -link-defaultlib-shared=false Optimizer.d -L--no-as-needed -L-lz -L-ldl
-
-REM VisualizerSVG:
-ldc2 -O3 -release -link-defaultlib-shared=false VisualizerSVG.d -L--no-as-needed -L-lz -L-ldl -L-lpthread
-
-REM VisualizerPNG:
-ldc2 -O3 -release -link-defaultlib-shared=false VisualizerPNG.d -L--no-as-needed -L-lpthread -L-lm -L-lrt -L-ldl -L-lz -of=VisualizerPNG
